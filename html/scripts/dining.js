05a241798bbb2581dce2e0e703a946687366c7fd
Ajay Gandhi <aag255@cornell.edu>
2015-03-13 11:14:45 -0400

    Day view for dining hours

--- html/scripts/dining.js
+++ html/scripts/dining.js
@@ -1,4 +1,16 @@
-$('.dining-calendar-column iframe').load( function() {
-    $('iframe').contents().find("head")
-      .append($("<style type='text/css'>  .my-class{display:none;}  </style>"));
+var date = new Date();
+var today = '' + date.getFullYear();
+if (date.getMonth() + 1 < 10) {
+  today += '0' + (date.getMonth() + 1);
+} else {
+  today += (date.getMonth() + 1);
+}
+today += date.getDate();
+console.log(today);
+
+$(document).ready(function() {
+  $('iframe')
+    .attr('src', $('iframe').attr('src') + '&mode=DAY&dates=' + today + '/' + today);
+  // $('iframe').contents().find('head')
+  //   .append($("<style type='text/css'> .my-class { display: none; } </style>"));
 });
