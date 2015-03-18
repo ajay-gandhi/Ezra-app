4b55e32dd4e699232309350c960fe98f216b1ae2
Ajay Gandhi <aag255@cornell.edu>
2015-03-18 11:15:52 -0400

    Modularize settings updates

--- html/scripts/settings.js
+++ html/scripts/settings.js
@@ -3,9 +3,12 @@
  */
 var ipc = require('ipc');
 
+// Maintain a local copy of all settings
 var settings = {
   remember: false
 }
+
+var update_timeout;
 
 // Change attributes based on settings object
 $(document).ready(function () {
@@ -24,11 +27,23 @@ $(document).ready(function () {
     .click(function () {
       settings.remember = !settings.remember;
       $(this).toggleClass('checked');
-
-      $.ajax({
-        url: 'http://127.0.0.1:3005/update-settings',
-        method: 'GET',
-        data: settings
-      });
+      update_settings();
     });
 });
+
+/**
+ * Conducts the AJAX call to the server to update the settings file
+ */
+var update_settings = function () {
+  // Conduct update after 1 second to prevent multiple web calls if numerous
+  // settings are being updated. In this fashion, everything in the settings
+  // object will be updated and the new settings will be sent to the server all
+  // at once.
+  update_timeout = window.setTimeout(function () {
+    $.ajax({
+      url: 'http://127.0.0.1:3005/update-settings',
+      method: 'GET',
+      data: settings
+    });
+  }, 1000);
+}
