c7ffbe3c0ccfc5526f4973c3f9789b61ceb83d7c
Ajay Gandhi <aag255@cornell.edu>
2015-04-13 13:05:47 -0400

    Add about page

--- app.js
+++ app.js
@@ -107,27 +107,38 @@ mainMenu.appendChild(helpMenu);
 
 var appleSubmenu = new Menu(application.name);
 appleSubmenu.appendChild(new MenuItem('About '+application.name, ''))
-    .addEventListener('click', function() { console.log('Do something for about!'); });
+  .addEventListener('click', function() {
+    // Display about window
+    var about_panel = new Panel();
+        about_panel.visible = true;
+        about_panel.title = 'About ' + application.name;
+        about_panel.appearance = 'dark';
+        about_panel.canBeFullscreen = false;
+        about_panel.width = 300;
+        about_panel.height = 400;
+        about_panel.x = (active.bounds.width / 2) - 150;
+        about_panel.y = (active.bounds.height / 2) - 200;
+
+    // Actual content
+    var about_webview = new WebView();
+        about_webview.location = 'app://html/about.html';
+        about_panel.appendChild(about_webview);
+  });
 appleSubmenu.appendChild(new MenuItemSeparator());
 appleSubmenu.appendChild(new MenuItem('Hide '+application.name, 'h'))
-    .addEventListener('click', function() { application.visible = false; });
+  .addEventListener('click', function() { application.visible = false; });
 appleSubmenu.appendChild(new MenuItem('Hide Others', ''))
-    .addEventListener('click', function() { application.hideAllOtherApplications(); });
+  .addEventListener('click', function() { application.hideAllOtherApplications(); });
 appleSubmenu.appendChild(new MenuItem('Show All', ''))
-    .addEventListener('click', function() { application.unhideAllOtherApplications(); });
+  .addEventListener('click', function() { application.unhideAllOtherApplications(); });
 appleSubmenu.appendChild(new MenuItemSeparator());
 appleSubmenu.appendChild(new MenuItem('Quit '+application.name, 'q'))
-    .addEventListener('click', function() { 
-      /* @hidden */ if(ismac) process.exit(0); 
-    });
+  .addEventListener('click', function() { 
+    /* @hidden */ if(ismac) process.exit(0);
+  });
 appleMenu.submenu = appleSubmenu;
 
 var fileSubmenu = new Menu('File');
-fileSubmenu.appendChild(new MenuItem('New File', 'f'));
-fileSubmenu.appendChild(new MenuItem('Open...', 'o'));
-fileSubmenu.appendChild(new MenuItem('Save', 's'));
-fileSubmenu.appendChild(new MenuItem('Save As...', 'S', 'shift'));
-fileSubmenu.appendChild(new MenuItemSeparator());
 fileSubmenu.appendChild(new MenuItem('Close', 'c', 'cmd'))
   .addEventListener('click', function() {
     /* @hidden */ if(!ismac) process.exit(0);
@@ -139,24 +150,22 @@ var undo = new MenuItem('Undo', 'u');
 undo.addEventListener('click', function() { application.undo(); });
 editSubmenu.appendChild(undo);
 editSubmenu.appendChild(new MenuItem('Redo', 'r'))
-    .addEventListener('click', function() { application.redo(); });
+  .addEventListener('click', function() { application.redo(); });
 editSubmenu.appendChild(new MenuItemSeparator());
 editSubmenu.appendChild(new MenuItem('Copy', 'c'))
-    .addEventListener('click', function() { application.copy(); });
+  .addEventListener('click', function() { application.copy(); });
 editSubmenu.appendChild(new MenuItem('Cut', 'x'))
-    .addEventListener('click', function() { application.cut(); });
+  .addEventListener('click', function() { application.cut(); });
 editSubmenu.appendChild(new MenuItem('Paste', 'p'))
-    .addEventListener('click', function() { application.paste(); });
+  .addEventListener('click', function() { application.paste(); });
 editMenu.submenu = editSubmenu;
 
 var windowSubmenu = new Menu('Window');
 windowSubmenu.appendChild(new MenuItem('Minimize', 'm'))
-    .addEventListener('click', function() { win.state = 'minimized'; });
-windowSubmenu.appendChild(new MenuItem('Zoom', ''))
-    .addEventListener('click', function() { win.state = 'maximized'; });
+  .addEventListener('click', function() { win.state = 'minimized'; });
 windowSubmenu.appendChild(new MenuItemSeparator());
 windowSubmenu.appendChild(new MenuItem('Bring All to Front', ''))
-    .addEventListener('click', function() { win.bringToFront(); });
+  .addEventListener('click', function() { win.bringToFront(); });
 windowSubmenu.appendChild(new MenuItemSeparator());
 windowMenu.submenu = windowSubmenu;
 
