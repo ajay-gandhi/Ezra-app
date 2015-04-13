7c410235566bb958e4aaec10a63a5980c0fed872
Ajay Gandhi <aag255@cornell.edu>
2015-04-13 15:52:50 -0400

    Update docs

--- html/scripts/app_messenger.js
+++ html/scripts/app_messenger.js
@@ -1,112 +1,126 @@
 'use strict';
-/* global $, console */
-
-/////////////////////////////////// LOGGING. ///////////////////////////////////
+/////////////////////////////////// LOGGING ////////////////////////////////////
 
 /**
- * Tint doesn't (yet) support developer tools on OSX. We can just route all logs
- * to a div in the acutal html document.
+ * Tint doesn't (yet) support developer tools on OSX, so route all logs to a div
+ * in the actual html document.
  */
-
-if (typeof console  != 'undefined') 
-  if (typeof console.log != 'undefined')
+if (typeof console  != 'undefined') {
+  if (typeof console.log != 'undefined') {
     console.olog = console.log;
-  else
+  } else {
     console.olog = function() {};
+  }
+}
 
 /**
- * Logs to the #log div. Note: at the moment supports only one argument.
+ * Redirects console logs to the #log div. Supports only one argument atm.
  */
 console.log = function(message) {
   // var args = Array.prototype.slice.call(arguments);
-  if ((typeof message) === 'function') message = '[function yo]';
+  if ((typeof message) === 'function') {
+    message = '[Cannot log function]';
+  }
+ 
   console.olog(message);
+
   try {
     $('#log').append('<p>' + JSON.stringify(message) + '</p>');
   } catch (e) {
     $('#log').append('<p>' + JSON.stringify(e) + '</p>');
     $('#log').append('<p>' + message + '</p>');
   }
-};
+}
 
 // All console functions log to #log
-console.error = console.debug = console.info =  console.log;
+console.error = console.debug = console.info = console.log;
 
 
 
-////////////////////////////////// MESSAGING. //////////////////////////////////
+////////////////////////////////// MESSAGING ///////////////////////////////////
 
 /**
- * Messenger handles node-webview communication using tint's message events. It
- * adds support for messaging serializable (JSON) objects to specific namespaces
+ * Messenger handles node-webview communication using tint's message events.
+ *    Adds support for sending serializable (JSON) objects to specific namespaces
  */
 function Messenger () {
-  
+
   var self = this;
   this.listeners = {};
 
   if (!window.postMessageToHost) {
-    console.log('!!! Not running on tint! Messages not supported');
+    console.log('Not running on tint! Messages not supported');
     return;
   }
 
   window.addEventListener('message', handle_message.bind(this));
 }
 
+/**
+ * Handles a message returned from the app. Ensures that the message conforms to
+ *   a certain format before calling the appropriate handler.
+ * @param {Object} message   The object returned from the app
+ */
 var handle_message = function(message) {
   try {
-    console.log('listeners: ' + JSON.stringify(Object.keys(this.listeners)));
-
     var msg = JSON.parse(message.data);
 
-    console.log(msg.namespace + ' -> ' + JSON.stringify(msg.body));
+    // Ensure returned message contains all necessary parts
+    if (!msg.namespace) {
+      console.log('No namespace damnit.');
+      return;
+    }
 
-    
-    if (!msg.namespace)
-      { console.log('No namespace damnit.'); return; }
-    
-    if (!msg.hasOwnProperty('body'))
-      { console.log('No body damnit.'); return; }
-    
-    if (!this.listeners[msg.namespace])
-      { console.log('No listener for ' + msg.namespace); return; }
+    if (!msg.hasOwnProperty('body')) {
+      console.log('No body damnit.');
+      return;
+    }
 
+    if (!this.listeners[msg.namespace]) {
+      console.log('No listener for ' + msg.namespace);
+      return;
+    }
+
+    // Call handler
     this.listeners[msg.namespace](msg.body);
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
 }
 
 /**
- * Registers a callback for a message recieved on namespace @param{namespace}
- * @param  {function} fun     TODO: function arguments
+ * Registers a handler for a message recieved on namespace @param{namespace}
+ * @param {string}   namespace   The namespace on which to expect a response
+ * @param {function} handler     The function to call when a message is
+ *   received on the given namespace
  */
-Messenger.prototype.recieve = function(namespace, funclol) {
-  this.listeners[namespace] = funclol;
+Messenger.prototype.recieve = function(namespace, handler) {
+  this.listeners[namespace] = handler;
 };
 
 /**
- * Sends a message. Response for request(x, _) should be expected with
- * recieve(x, _), for the same namespace string x. It's kinda like http, at
- * least in the way we use it on this app.
+ * Sends a message to the app. The response for request(x, _) should be expected
+ * with receive(x, _), for the same namespace string x. It's usage resembles
+ * http in this app.
+ * @param {string} namespace   The namespace on which to send the request
+ * @param {Object} body        Parameters to send to the app
  */
 Messenger.prototype.request = function(namespace, body) {
   if (body === null) body = {};
 
-  // console.log(namespace + ' <- ' + JSON.stringify(body));
+  // Format request
   var msg = JSON.stringify({
     namespace : namespace,
     body : body
   });
 
   if (!window.postMessageToHost) {
-    console.log('x nope');
+    console.log('Not running on tint! Messages not supported');
     return;
   }
 
+  // Send request
   window.postMessageToHost(msg);
 };
 
-
-window.coolio = new Messenger();
-
+window.messenger = new Messenger();
